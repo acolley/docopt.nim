a109a78fdf4edff38c33209fc48951e503ecf6eb
Alasdair Colley <alnessy@hotmail.com>
2013-11-06 18:48:55 +0100

    more functionality (now sort of parses TOptions)

--- docopt.nim
+++ docopt.nim
@@ -1,57 +1,105 @@
 import os
-import re
+import re except match
+import sequtils
 import strutils
 import tables
-
-from sequtils import zip
 
 type
   EDocoptLanguageError = object of E_Base
 
   # nimrod's algebraic datatypes are 'case objects'
   # represents option types
-  TOptionKind* = enum
-    OptString,
-    OptSwitch
+  #TOptionKind* = enum
+  #  OptString,
+  #  OptSwitch
 
-  TOption* = object
-    case kind: TOptionKind
-    of OptString:
-      valStr: string
-    of OptSwitch:
-      valBool: bool
+  #TOption* = object
+  #  case kind: TOptionKind
+  #  of OptString:
+  #    valStr: string
+  #  of OptSwitch:
+  #    valBool: bool
 
-  TPattern = object
+  # NOTE: can also use {.inheritable.} and not have to inherit
+  # from TObject to allow inheritance
+  TPattern = ref object of TObject
 
-# these are just ripped from the standard library
-# with extra 'maxsplit' feature
-# TODO: contribute them back to the std lib
-iterator mySplit(s: string, sep: char, maxsplit=high(int)): string =
-  var last = 0
-  assert('\0' != sep)
-  if len(s) > 0:
-    var numsplits = 0
-    # `<=` is correct here for edge cases
-    while last <= len(s) and numsplits < maxsplit:
-      var first = last
-      while last < len(s) and s[last] != sep: inc(last)
-      inc(numsplits)
-      yield substr(s, first, last-1)
-      inc(last)
+  TBranchPattern = ref object of TPattern
+    children: seq[TPattern]
 
-    if last < len(s):
-      yield substr(s, last, len(s) - 1)
+  TLeafPattern = ref object of TPattern
+    name, value: string
 
-proc mySplit(s: string, sep: char, maxsplit=high(int)): seq[string] =
-  accumulateResult(mySplit(s, sep, maxsplit))
+  TArgument = ref object of TLeafPattern
+  TCommand = ref object of TArgument
 
-proc partition(s: string, sep: char): tuple[pre: string, sep: string, post: string] =
-  let parts = mySplit(s, sep, maxsplit=1)
+  TOption = ref object of TLeafPattern
+    short, long: string
+    argcount: int
+
+  TRequired = ref object of TBranchPattern
+
+  TOptional = ref object of TBranchPattern
+  TOptionsShortcut = ref object of TOptional
+
+  TOneOrMore = ref object of TBranchPattern
+
+  TEither = ref object of TBranchPattern
+
+# TPattern implementation
+
+method `$`(patt: TPattern): string =
+  result = "TPattern"
+
+#method fix(patt: ref TBranchPattern): ref TPattern =
+# fix identities
+
+# fix repeating arguments
+
+# END TPattern implementation
+
+# TLeafPattern implementation
+method name(patt: TLeafPattern): string =
+  result = patt.name
+
+method `$`(patt: TLeafPattern): string =
+  result = patt.name
+# END TLeafPattern implementation
+
+# TOption implementation
+proc parseOption(optdesc: string): TOption =
+  var
+    short = ""
+    long = ""
+    argcount = 0
+    value = ""
+
+  var (options, ign, desc) = optdesc.strip().partition("  ")
+  options = options.replace(",", " ").replace("=", " ")
+  for s in options.split(' '):
+    if s.startswith("--"):
+      long = s
+    elif s.startswith("-"):
+      short = s
+    else:
+      argcount = 1
+
+  if argcount > 0:
+    let matched = findAll(desc, re(r"\[default: (.*)\]", {reIgnoreCase}))
+    if len(matched) > 0:
+      value = matched[0]
   
-  if len(parts) > 1:
-    result = (parts[0], $sep, parts[1])
+  result = TOption(short: short, long: long, argcount: argcount, value: value)
+
+#method single_match(opt: TOption, left: seq[TPattern])
+
+method name(opt: TOption): string =
+  if opt.long != "":
+    result = opt.long
   else:
-    result = (s, "", "")
+    result = opt.short
+
+# END TOption implementation
 
 iterator walk[T](s: seq[T], stride=1, start=0): T =
   ## walk through a sequence with given stride
@@ -64,9 +112,7 @@ iterator walk[T](s: seq[T], stride=1, start=0): T =
 proc walk[T](s: seq[T], stride=1, start=0): seq[T] =
   accumulateResult(walk(s, stride, start))
 
-#proc parse(opt: 
-
-#proc match(pattern: TPattern): tuple[res: bool, left: seq[TPattern], coll: seq[TPattern]] =
+#proc match(pattern: ref TPattern): tuple[res: bool, left: seq[TPattern], coll: seq[TPattern]] =
 #  result = nil
 
 #proc parse_pattern(source: string, options: seq[string]): TPattern =
@@ -74,25 +120,34 @@ proc walk[T](s: seq[T], stride=1, start=0): seq[T] =
 
 proc parseSection(name: string, source: string): seq[string] =
   result = @[]
+  let found = findAll(source, re(r"^([^\n]*" & name & r"[^\n]*\n?(?:[ \t].*?(?:\n|$))*)", {reIgnoreCase, reMultiLine}))
+  for s in found:
+    result = result & strip(s)
 
-proc parse_defaults(doc: string): seq[string] =
+proc parseDefaults(doc: string): seq[TOption] =
   result = @[]
   for s in parse_section("options:", doc):
     # FIXME corner case "bla: options: --foo
     let post = partition(s, ':')[2] # get rid of "options:"
+    
+    # docopt.py regex is "\n[ \t]*(-\S+?)" the reason for the
+    # positive lookahead here is that Nimrod's regexes don't
+    # return elements that were in matching parens
+    # TODO: maybe a useful addition to re.nim?
+    var splitStr = split("\n" & post, re(r"\n[ \t]*(?=-\S+?)", {}))
+    splitStr = splitStr[1..len(splitStr)-1]
+    #var elems: seq[string] = @[]
+    #for stup in zip(walk(splitStr, stride=2), walk(splitStr, stride=2, start=1)):
+    #  elems = elems & (stup[0] & stup[1])
 
-    let splitStr = split("\n" & post, re"\n[ \t]*(-\S+?)")
-    var elems: seq[string] = @[]
-    for stup in zip(walk(splitStr), walk(splitStr, start=1)):
-      elems = elems & (stup[0] & stup[1])
+    result = result & map(filter(splitStr, proc(s: string): bool = s.startswith("-")), parseOption)
 
-
-
-proc formal_usage(s: string): string =
-  var section = mySplit(s, ':')[1]
+proc formalUsage(s: string): string =
+  var section = split(s, ':')[1] # drop "usage:"
+  let pu = section.split(' ')
   result = section
 
-proc docopt*(doc: string, argv: seq[string]=nil, help=true, version="", options_first=false): TTable[string, string] =
+proc docopt*(doc: string, argv: seq[string]=nil, help=true, version="", optionsFirst=false): TTable[string, string] =
   result = initTable[string, string]()
 
   var args = argv
@@ -102,11 +157,11 @@ proc docopt*(doc: string, argv: seq[string]=nil, help=true, version="", options_
       args = args & paramStr(i)
 
   let usageSections = parseSection("usage:", doc)
-  if usageSections.len() == 0:
+  if len(usageSections) == 0:
     raise newException(EDocoptLanguageError, "\"usage:\" (case-insensitive) not found.")
-  if usageSections.len() > 1:
+  if len(usageSections) > 1:
     raise newException(EDocoptLanguageError, "More than one \"usage:\" (case-insensitive).")
   # TODO: DocoptExit.usage = usage_sections[0]
 
-  let options = parse_defaults(doc)
+  let options = parseDefaults(doc)
   #let pattern = parse_pattern(formal_usage(usage_sections[0]), options)
--- test.nim
+++ test.nim
@@ -1,5 +1,24 @@
 import docopt
 
+let teststr = """
+Naval Fate.
+
+Usage:
+  naval_fate.py ship new <name>...
+  naval_fate.py ship <name> move <x> <y> [--speed=<kn>]
+  naval_fate.py ship shoot <x> <y>
+  naval_fate.py mine (set|remove) <x> <y> [--moored | --drifting]
+  naval_fate.py (-h | --help)
+  naval_fate.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Moored (anchored) mine.
+  --drifting    Drifting mine.
+
+"""
 
 when isMainModule:
-  discard docopt("", @["hey", "boys"])
+  discard docopt(teststr, @["hey", "boys"])
